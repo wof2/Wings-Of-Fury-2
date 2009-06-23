2a6f92b75f16fdf26c230c1be2f278cc9b135cc7
singaja <singaja@2eb068c9-58a9-4cf6-a8ac-5e0c5f450555>
2009-06-23 20:01:57 +0000

    Sprawdzanie czy jest .NET runtime 2.0 w launcherze

--- dev/src/Launcher/Launcher.cpp
+++ dev/src/Launcher/Launcher.cpp
@@ -4,6 +4,50 @@
 #include "stdafx.h"
 #include <shellapi.h>
 
+const TCHAR *g_szNetfx20RegKeyName = _T("Software\\Microsoft\\NET Framework Setup\\NDP\\v2.0.50727");
+const TCHAR *g_szNetfxStandardRegValueName = _T("Install");
+
+bool RegistryGetValue(HKEY, const TCHAR*, const TCHAR*, DWORD, LPBYTE, DWORD);
+bool IsNetfx20Installed();
+
+bool RegistryGetValue(HKEY hk, const TCHAR * pszKey, const TCHAR * pszValue, DWORD dwType, LPBYTE data, DWORD dwSize)
+{
+	HKEY hkOpened;
+
+	// Try to open the key
+	if (RegOpenKeyEx(hk, pszKey, 0, KEY_READ, &hkOpened) != ERROR_SUCCESS)
+	{
+		return false;
+	}
+
+	// If the key was opened, try to retrieve the value
+	if (RegQueryValueEx(hkOpened, pszValue, 0, &dwType, (LPBYTE)data, &dwSize) != ERROR_SUCCESS)
+	{
+		RegCloseKey(hkOpened);
+		return false;
+	}
+
+	// Clean up
+	RegCloseKey(hkOpened);
+
+	return true;
+}
+
+bool IsNetfx20Installed()
+{
+	bool bRetValue = false;
+	DWORD dwRegValue=0;
+
+	if (RegistryGetValue(HKEY_LOCAL_MACHINE, g_szNetfx20RegKeyName, g_szNetfxStandardRegValueName, NULL, (LPBYTE)&dwRegValue, sizeof(DWORD)))
+	{
+		if (1 == dwRegValue)
+			bRetValue = true;
+	}
+
+	return bRetValue;
+}
+
+
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPTSTR    lpCmdLine,
@@ -13,11 +57,19 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	char dirFilename[255];
 	GetCurrentDirectoryA(255,dir);
 
+	if(!IsNetfx20Installed())
+	{
+		MessageBox(0,L".NET runtime 2.0 is required.",L"Wings of Fury launching error!",MB_ICONINFORMATION);
+	}
+
 	strcat(dir,"\\bin\\Release");
 	strncpy(dirFilename,dir,255);
 	strcat(dirFilename,"\\Wof.exe");
 
 	ShellExecuteA(0,"open",dirFilename,"",dir,SW_SHOWNORMAL);
+
+	/*
+	//Wysy³anie WM_CLOSE do okienka eziriza
 
 	HWND found;
 	for(int i = 0 ; i < 10 ; i++)
@@ -28,6 +80,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	}
 	PostMessage(found,WM_CLOSE,0,0);
 	return 0;
+	*/
 }
 
 
