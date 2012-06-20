e2d99c2dc9c9a5151625a35589060b429949ffbb
codo <codo@2eb068c9-58a9-4cf6-a8ac-5e0c5f450555>
2012-06-20 23:53:46 +0000

    testss

---
+++ dev/Wingitor/Tests/ExplosionsGameTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using wingitor;
+using Wingitor;
+using Wof.Controller;
+using Wof.Model.Level.XmlParser;
+
+namespace wingitor.Tests
+{
+    public class ExplosionsGameTest : IGameTest
+    {
+        readonly List<ISceneTest> sceneTests = new List<ISceneTest> { new ExplosionTestScene() };
+
+        public string LevelFilename
+        {
+            get { return "custom_levels/enhanced-4" + XmlLevelParser.C_LEVEL_POSTFIX; }
+          //  get { return "levels/level-1" + XmlLevelParser.C_LEVEL_POSTFIX; }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainWindow mw = new MainWindow(new ExplosionsGameTest());
+            Application.Run(mw);
+        }
+
+        #region Implementation of IGameTest
+
+        public ExplosionsGameTest()
+        {
+           
+        }
+       
+        public IList<ISceneTest> SceneTests
+        {
+            get { return sceneTests; }
+        }
+
+        #endregion
+
+      
+
+    }
+}
